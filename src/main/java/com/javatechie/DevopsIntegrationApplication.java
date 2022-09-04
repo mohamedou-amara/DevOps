b1096ed23114729d9596dcdfa79f8b6ce6c91e6d
mohamedou-amara <93713579+mohamedou-amara@users.noreply.github.com>
2022-09-04 18:44:30 +0100

    Update DevopsIntegrationApplication.java

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "welcome to javatechie";
+		return "test app for internship ";
 	}
 
 	public static void main(String[] args) {
